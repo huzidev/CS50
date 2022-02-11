bd9713d0d193bc8106a5416e3f8a9aa1be3586eb
huzidev <huzaifa.iqdev@gmail.com>
2022-02-11 22:00:39 +0500

    merge sort

---
+++ C/DSA/MergeSort/MergeSort.c
@@ -0,0 +1,110 @@
+#include <stdio.h>
+
+// i is for left, j is for right and k is for new array in which all the sorted array will be insert
+
+void merge(int arr[], int beg, int mid, int end){
+	
+	int len1 = mid - beg + 1;
+//	printf("%i ", len1);
+	
+	int len2 = end - mid;
+	
+	int LeftArr[len1], RightArr[len2];
+	
+	for( int i = 0; i < len1; i++){
+		
+		LeftArr[i] = arr[beg + i];
+		
+	}
+	
+	for( int j = 0; j < len2; j++){
+		
+		RightArr[j] = arr[mid + 1 + j ];
+		
+	}
+	
+	int i, j, k;
+	
+	i = 0; 
+	
+	j = 0;
+	
+	k = beg; // bcz all the sorted elements will be in that array therefore BEG
+	
+	while( i < len1 && j < len2){
+		
+		if( LeftArr[i] <= RightArr[j]){
+			
+			arr[k] = LeftArr[i];
+			i++;
+			
+		}
+		else{
+			
+			arr[k] = RightArr[j];
+			j++;
+			
+		}
+		k++; // so after taking an element we'll move toward next index in new array so k will be available at next for new element
+	}
+	
+	while( i < len1){
+		
+		arr[k] = LeftArr[i];
+		i++;
+		k++;
+		
+	}
+	while( j < len2){
+		
+		arr[k] = RightArr[j];
+		j++;
+		k++;
+		
+	}
+	
+}
+
+
+void MergeSort(int arr[], int beg, int end){
+	
+	if( beg < end){
+		
+		int mid = beg + ( end - beg ) / 2;
+		MergeSort(arr, beg, mid); // for left array
+		MergeSort(arr, mid + 1, end); // for right array
+		merge(arr, beg, mid, end); // for merging left and right
+		
+	}
+}
+
+void display(int arr[], int size){
+	
+	for( int i = 0; i <= size; i++){
+		
+		printf("%d ", arr[i]);
+		
+	}
+	printf("\n");
+}
+
+int main(void){
+	
+	int arr[] = {4, 6, 9, 21, 36, 1, 0, 48, 34, 321};
+	
+	int size = (sizeof(arr) / sizeof(int));
+	
+	printf("Total elements are %i\n", size - 1);
+	
+	printf("Before Sorting: ");
+	
+	display(arr, size);
+	
+	printf("\nAfter Sorting: ");
+	
+	MergeSort(arr, 0, size);
+	
+	display(arr, size);	
+	
+	return 0;
+}
---
+++ C/DSA/QuickSort/QuickSort.c
@@ -0,0 +1,77 @@
+#include <stdio.h>
+
+void SwapElements(int *first, int *second){
+	
+	int temp = *first;
+	
+	*first = *second;
+	
+	*second = temp;
+}
+
+int Partition(int arr[], int beg, int end){
+	
+	int pivot = arr[end];
+	
+	int pIndex = (beg - 1);
+	
+	for( int i = beg; i < end; i++){ // why we said < end is because the last one is ours PIVOT 
+		
+		if ( arr[i] < pivot ){ // 14, 17, 8, 90, 11, 2 there 2 is ours PIVOT
+			
+			pIndex++; // coz it is at -1 index
+
+			SwapElements(&arr[pIndex], &arr[i]);
+		}
+		
+	}// remember the comparsion will be with every element with the PIVOT one because of FOR loop
+	
+	
+	SwapElements(&arr[pIndex + 1 ], &arr[end]); // now if ours arr[i] didn't result in lesser than any element we just swap the beg and end with each other
+	
+	return (pIndex + 1); // since it is a recursive function therefore we says return coz recursive functions call itself again and again	
+}
+
+void QuickSort(int arr[], int beg, int end){
+	
+	if( beg < end){
+		
+		int pIndex = Partition(arr, beg, end);
+		
+		QuickSort(arr, beg, pIndex - 1);
+		QuickSort(arr, pIndex + 1, end);
+		
+	}
+	
+}
+
+void Print(int arr[], int size){
+	
+	for(int i = 0; i < size; i++){
+		
+		printf("%d ", arr[i]);
+		
+	}
+	
+}
+
+int main(void){
+	
+	int arr[] = {14, 17, 8, 90, 11, 2};
+	
+	int size = (sizeof(arr) / sizeof(int));
+	
+	printf("Total number of elements are %i\n", size - 1);
+	
+	printf("Before sorting: ");
+	
+	Print(arr, size);
+	
+	printf("\nAfter sorting: ");
+	
+	QuickSort(arr, 0, size);
+	
+	Print(arr, size);
+	
+	return 0;
+}
