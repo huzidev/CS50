28b77744766f17c06aa8436a90625e132412e39a
huzidev <huzaifa.iqdev@gmail.com>
2022-02-17 11:00:01 +0500

    linear search fix

--- C/DSA/LinearSearch/LinearSearch.c
+++ C/DSA/LinearSearch/LinearSearch.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
-
-
+#include <cs50.c>
 
 int linearSearch(int arr[], int TargetElement, int size){
 	
@@ -8,7 +7,7 @@ int linearSearch(int arr[], int TargetElement, int size){
 		
 		if(arr[i] == TargetElement){
 			
-			return i; 
+			return i; // just like we return mid in binarysearch when arr[mid] == item
 			
 		}
 	}	
@@ -21,15 +20,17 @@ int main(void){
 	
 	int arr[] = {65 ,99, 32, 1, 0, 3, 48, 7};
 	
-	int TargetElement = 3;
-	
 	int size = (sizeof(arr) / sizeof(int));
 	
 	printf("total elements are %i\n", size - 1); // for index we says size - 1;
 	
+	printf("Which element you wanna search in bw them : 65 ,99, 32, 1, 0, 3, 48, 7\n");
+	
+	int TargetElement = get_int("Enter the element you wanna search : ");
+	
 	int ans = linearSearch(arr, TargetElement, size); // will stores ours answer
 	
-	if( ans == -1){
+	if( ans == -1 ){
 		
 		printf(" Element is not present in the given array!");
 
@@ -41,19 +42,6 @@ int main(void){
 		
 	}
 	
-	TargetElement = 100; // which is basically not presenet
-	
-	ans = linearSearch(arr, TargetElement, size);
-	
-	if( ans == -1){
-		
-		printf("Element is not present in the given array\n");
-		
-	}
-	else{
-		
-		printf("Element is found at index numebr %dth of array \n", ans);
-		
-	}	
+	return 0;
 }
 	
