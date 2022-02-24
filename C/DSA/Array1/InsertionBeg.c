2f1dcd6a5f9a20cf6b8d5cd81102c7c1d192afd5
huzidev <huzaifa.iqdev@gmail.com>
2022-02-24 11:47:28 +0500

    array beg

--- C/DSA/Array1/InsertionBeg.c
+++ C/DSA/Array1/InsertionBeg.c
@@ -4,39 +4,43 @@
 
 int main(void){
 	
-	int arr[] = {5, 6, 9, 10, 100};
+	int arr[5];
+	int data;
+	int i, n;	
 	
-	int size = (sizeof(arr) / sizeof(int));	
+	n = get_int("how many elements you wanna print? ");
 	
-	printf("total elements were %d\n", size-1);
-	
-	printf("before insertion of element at beg : ");
-	
-	for(int i = 0; i < size; i++){
+	if ( n >= 5 ){
 		
-		printf("%d ", arr[i]);	
+		printf("You have to insert less than 5 int");
 		
 	}
 	
-	size = add_beg(arr, size);
-	
-	for(int i = 0; i < size+1; i++){
+	else{
 		
-		printf("%d ", arr[i]);
 		
+		for(int i = 0; i < n; i++){
+			
+			arr[i] = get_int("value : ");
+			
+		}
+		
+		n = add_beg(arr, n, data);
+		
+		for(int i = 0; i <= n; i++){ // so updated values at n shall also work
+			
+			printf("%d ", arr[i]);
+			
+		}
 	}
-	
 	return 0;
-	
 }
 
-int add_beg(int arr[], int size){
+int add_beg(int arr[], int n, int data){
 	
-	int data;
-	
-	for(int i = size-1; i >= 0; i--){
+	for(int i = n-1; i >= 0; i--){ // coz n is the empty slot available and n-1 is the last ever value i>=0 so when i get into -ve index we simply break the loops
 		
-		arr[i+1] = arr[i];
+		arr[i+1] = arr[i]; // suppose if n = i-1 means if n is at 4th index then i should have to be at 3rd index but we wanted to insert the value of 3rd index to 4th index like swapping
 		
 	}
 	
@@ -44,6 +48,6 @@ int add_beg(int arr[], int size){
 	
 	arr[0] = data;
 	
-	return size+1;
-	
+	return n;
+		
 }
