94d19ebe31ac0d9a122fabba0393d5a3831c413c
huzidev <huzaifa.iqdev@gmail.com>
2022-02-26 15:11:38 +0500

    first element beg

---
+++ C/DSA/Array1/DeleteEnd.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <cs50.c>
+
+// for deletetion at beg in case of array we simple did size = size-1
+int main(void){
+	
+	int arr[] = {1, 5, 6, 10, 15, 20, 25};
+	
+	int size = (sizeof(arr)) / sizeof(int);	
+	
+	printf("Before deletion at beg : ");
+	
+	for(int i = 0; i < size; i++){
+		
+		printf("%d ", arr[i]);
+		
+	}
+	
+	size = size - 1;
+	
+	printf("\nAfter deletion at beg : ");
+	
+	for(int i = 0; i < size; i++){
+		
+		printf("%d ", arr[i]);
+		
+	}
+	
+	return 0;
+}
