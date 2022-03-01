98f472e65f49d56bd7d7feda0e53759b17e92f9a
huzidev <huzaifa.iqdev@gmail.com>
2022-03-01 20:32:20 +0500

    pointer

--- C/DSA/LinkList1/DoublyLinkList/InsertEndDoubly.c
+++ C/DSA/LinkList1/DoublyLinkList/InsertEndDoubly.c
@@ -11,7 +11,7 @@ struct node{
 	
 	struct node *next;
 	
-}*head, *tmp1;
+}*head, *tmp, *tmp1;
 
 struct node* addToEmpty(struct node *head){
 	
@@ -55,6 +55,40 @@ struct node* AddAtBeg(struct node *head){
 	head->prev = tmp1;
 	
 	head = tmp1;
+	
+	return head;
+	
+}
+
+struct node* AddAtEnd(struct node *head){
+	
+	int data;
+	
+	struct node *tmp1 = NULL;
+	
+	struct node *tmp = NULL;
+	
+	tmp = head;
+	
+	tmp1 = (struct node*)malloc(sizeof(struct node));
+	
+	tmp1->prev = NULL;
+	
+	data = get_int("Element you wanna insert at End : ");
+	
+	tmp1->data = data;
+	
+	tmp1->next = NULL;
+	
+	while(tmp->next != NULL){
+		
+		tmp = tmp->next;
+		
+	}
+	
+	tmp1->prev = tmp;
+	
+	tmp->next = tmp1;
 	
 	return head;
 	
