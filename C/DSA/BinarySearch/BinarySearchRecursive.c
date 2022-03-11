d346ca7ebfb6d1df4691538e7729a89df7f8768c
huzidev <huzaifa.iqdev@gmail.com>
2022-03-11 21:46:42 +0500

    binary search recursive

--- C/DSA/BinarySearch/BinarySearchRecursive.c
+++ C/DSA/BinarySearch/BinarySearchRecursive.c
@@ -19,7 +19,7 @@ int BinarySearch(int arr[], int item, int beg, int end){
 			
 			// basically we did completely right on iterative we says end = midindex - 1; thier we replaced end with midindex - 1
 		
-			return BinarySearch(arr, item, beg, midIndex - 1); // this is call RECUSION call the main function again & again
+			return BinarySearch(arr, item, beg, midIndex - 1); // this is call RECURSION call the main function again & again
 
 			// since BinarySearch is the name of ours main function therefore we're calling or making a recursive calls			
 	
@@ -37,28 +37,56 @@ int BinarySearch(int arr[], int item, int beg, int end){
 
 int main(void){
 	
-	int arr[] = {25, 35, 38, 65, 70, 75, 80, 99, 100};
+	int arr[] = {10, 25, 35, 40, 55, 65, 79, 99, 100};
 	
 	int size = (sizeof(arr) / sizeof(int));
 	
 	printf("The total number of elements are %i\n", size - 1);
 	
-	printf("List of elements in the arrays is : 25, 35, 38, 65, 70, 75, 80, 99, 100\n");
+	printf("Which element you wanna search in bw them? : ");
 	
-	int item = get_int("Which element you wanna search : ");
+	for( int i = 0; i < size; i++){
+		
+		printf("%d ", arr[i]);
+		
+	}
+	
+	int item = get_int("\nWhich element you wanna search : ");
 	
 	int ans = BinarySearch(arr, item, 0, size); // 0 means beg of index	
 	
-	if ( ans == -1 ){
+	if( ans == -1 ){
 		
-		printf("The element you searched for isn't present in the given array\n");
-		
+		printf(" Element is not present in the given array!");
+
 	}
+
 	else{
 		
-		printf("the element you search for is %dth element of array\n", ans);
+		if( item == arr[1] ){
+			
+			printf("Element is found at index numebr %dst of array \n", ans);
+			
+		}
+		
+		else if( item == arr[2] ){
+			
+			printf("Element is found at index numebr %dnd of array \n", ans);
+			
+		}
+		
+		else if( item == arr[3] ){
+			
+			printf("Element is found at index numebr %drd of array \n", ans);
+			
+		}
+		else{
+		
+			printf("Element is found at index numebr %dth of array \n", ans);
+		
+		}
+		
 		
 	}
-	
 	return 0;
 }
