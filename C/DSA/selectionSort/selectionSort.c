acbf75c78418532eb9e7797c2be094287315b5f9
huzidev <huzaifa.iqdev@gmail.com>
2022-03-14 16:41:25 +0500

    selection sort

--- C/DSA/selectionSort/selectionSort.c
+++ C/DSA/selectionSort/selectionSort.c
@@ -1,25 +1,29 @@
 #include <stdio.h>
 #include <cs50.c>
 
+// selection sort is just opposite of bubble sort in bubble sort we send all the bigger elements at right in selection sort we send all the smaller elements at left 
+
 void selectionSort(int arr[], int size){
 	
-	for(int j = 0; j < size - 1; j++){ // size - 1 is becasuse we'll have j and j+1 for swapping by comparision now when we came to all over the end the last element the j will be their but J+1 will points toward NULL therefore iterate till size - 1
+	// if size is 5 then number of swaps will just be 4 therefore size - 1
+	
+	for(int j = 0; j < size-1; j++){ // size - 1 is becasuse we'll have j and j+1 for swapping by comparision now when we came to all over the end the last element the j will be their but J+1 will points toward NULL therefore iterate till size - 1
 		
 		int min = j; // j will be the 0th index (element) the first ever element	
 		
-		for(int i = j + 1; i < size; i++ ){ // i = j+1 is because   10 , 6, 5, 4, 1, 2
-//																  	 j   i
+		for(int i = j + 1; i < size; i++ ){ // i = j+1 is because   10 , 6, 5, 4, 1, 2 and we'll compare it all the way till end so we can compare all the elements
+//																  	 j   i // if j is at 0th index then i will be at j+1 index
 // if j is minimum first index then i will be next index hence j + 1			
 			
 			if(arr[i] < arr[min]){ // min is J and i is j + 1 // FOR DESCENDING ORDER CHANGE THE SIGN
 				
-				min = i;  // coz if i is smaller and min which is j is bigger than swap by (COMPARISION)
+				min = i;  // coz if i is smaller and min which is j is bigger then make new minimum element and swap it with all the other remaining elements
 				
 			}
 		}		
 			int temp = arr[j]; // since j is at 0th index hence we'll swap elemens of min with j
 			
-			arr[j] = arr[min];
+			arr[j] = arr[min]; // so element at 1st index which is smaller than element at 0th index therefore we'll swap it by the 0th index element
 			
 			arr[min] = temp;
 	}
