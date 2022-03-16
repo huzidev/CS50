16f214b797bb014f8502832e2056892a34d5aaf2
huzidev <huzaifa.iqdev@gmail.com>
2022-03-16 22:16:31 +0500

    3D-Array

---
+++ C/DSA/Array1/3D-Array/3D-Array.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <cs50.c>
+
+int main(void){
+	
+	int parts = get_int("How many 2D Array you want? ");
+	
+	int row = get_int("How many rows you want? ");
+	
+	int columns = get_int("How many columns you want? ");
+	
+}
