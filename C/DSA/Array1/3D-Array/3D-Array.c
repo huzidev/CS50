6868db5ce3cbe152c8dab1b327bd0f0210668e3f
huzidev <huzaifa.iqdev@gmail.com>
2022-03-18 07:18:29 +0500

    2d arrays for 3d arrays

--- C/DSA/Array1/3D-Array/3D-Array.c
+++ C/DSA/Array1/3D-Array/3D-Array.c
@@ -307,29 +307,55 @@ int main(void){
 					
 				}	
 				
-				// for 1st 2D-array 3rd row and 0 to 3rd columns	
+				// for 2nd 2D-array 3rd row and 0 to 3rd columns	
 						
-				else if( i == 3 && j == 3 && k == 0 ){
+				else if( i == 2 && j == 3 && k == 0 ){
 					
-					arr[i][j][k] = get_int("Value for %drd 2D-array's %drd row's %dth column : ", i, j, k);
-					
-				}
-				
-				else if( i == 3 && j == 3 && k == 1 ){
-					
-					arr[i][j][k] = get_int("Value for %drd 2D-array's %drd row's %dst column : ", i, j, k);
+					arr[i][j][k] = get_int("Value for %dnd 2D-array's %drd row's %dth column : ", i, j, k);
 					
 				}
 				
-				else if( i == 3 && j == 3 && k == 2 ){
+				else if( i == 2 && j == 3 && k == 1 ){
 					
-					arr[i][j][k] = get_int("Value for %drd 2D-array's %drd row's %dnd column : ", i, j, k);
+					arr[i][j][k] = get_int("Value for %dnd 2D-array's %drd row's %dst column : ", i, j, k);
 					
 				}
 				
-				else if( i == 3 && j == 3 && k == 3 ){
+				else if( i == 2 && j == 3 && k == 2 ){
 					
-					arr[i][j][k] = get_int("Value for %drd 2D-array's %drd row's %drd column : ", i, j, k);
+					arr[i][j][k] = get_int("Value for %dnd 2D-array's %drd row's %dnd column : ", i, j, k);
+					
+				}
+				
+				else if( i == 2 && j == 3 && k == 3 ){
+					
+					arr[i][j][k] = get_int("Value for %dnd 2D-array's %drd row's %drd column : ", i, j, k);
+					
+				}
+				
+				// for 3rd 2D-array 0th row and 0 to 3rd columns	
+						
+				else if( i == 3 && j == 0 && k == 0 ){
+					
+					arr[i][j][k] = get_int("Value for %drd 2D-array's %dth row's %dth column : ", i, j, k);
+					
+				}
+				
+				else if( i == 3 && j == 0 && k == 1 ){
+					
+					arr[i][j][k] = get_int("Value for %drd 2D-array's %dth row's %dst column : ", i, j, k);
+					
+				}
+				
+				else if( i == 3 && j == 0 && k == 2 ){
+					
+					arr[i][j][k] = get_int("Value for %drd 2D-array's %dth row's %dnd column : ", i, j, k);
+					
+				}
+				
+				else if( i == 3 && j == 0 && k == 3 ){
+					
+					arr[i][j][k] = get_int("Value for %drd 2D-array's %dth row's %drd column : ", i, j, k);
 					
 				}
 			}
