7600b60ab144ed2ac839c2efdcbbbd37d63f7a22
huzidev <huzaifa.iqdev@gmail.com>
2022-03-19 07:32:49 +0500

    even and odd

--- C/DSA/Array1/2D-Array/2D-ArrayProblem.c
+++ C/DSA/Array1/2D-Array/2D-ArrayProblem.c
@@ -60,13 +60,25 @@ int main(void){
 			
 		}
 		
-		if( ans == 'D' ){
+		else if( ans == 'D' ){
 			
 			printf("\nAfter appyling algorithm! : ");
 			
 			for(int i = 0; i < 1; i++){
 				
 				printf("%d %d %d %d %d ", arr[4][4], arr[3][3], arr[2][2], arr[1][1], arr[0][0]);
+				
+			}
+			
+		}
+		
+		else if( ans == 'E' ){
+			
+			printf("\nAfter appyling algorithm! : ");
+			
+			for(int i = 0; i < 1; i++){
+				
+				printf("%d %d %d %d %d ", arr[0][0], arr[2][2], arr[4][4]);
 				
 			}
 			
