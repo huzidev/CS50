a6aef1d6b6f1a3ae91c0c9a2385fb061b5a0a08f
huzidev <huzaifa.iqdev@gmail.com>
2022-03-19 08:20:14 +0500

    even odd fix

--- C/DSA/Array1/2D-Array/2D-ArrayProblem.c
+++ C/DSA/Array1/2D-Array/2D-ArrayProblem.c
@@ -82,7 +82,7 @@ int main(void){
 			
 			for(int i = 0; i < 1; i++){
 				
-				printf("%d %d %d ", arr[0][0], arr[0][1], arr[0][2], arr[0][3], arr[0][4], arr[2][0], arr[2][1], arr[2][2], arr[2][3], arr[2][4], arr[4][0], arr[4][1], arr[4][2], arr[4][3], arr[4][4]);
+				printf("%d %d %d %d %d %d %d %d %d %d %d %d %d %d %d ", arr[0][0], arr[0][1], arr[0][2], arr[0][3], arr[0][4], arr[2][0], arr[2][1], arr[2][2], arr[2][3], arr[2][4], arr[4][0], arr[4][1], arr[4][2], arr[4][3], arr[4][4]);
 				
 			}
 			
@@ -94,7 +94,7 @@ int main(void){
 			
 			for(int i = 0; i < 1; i++){
 				
-				printf("%d %d ", arr[0][0]);
+				printf("%d %d %d %d %d %d %d %d %d %d ", arr[1][0], arr[1][1], arr[1][2], arr[1][3], arr[1][4], arr[3][0], arr[3][1], arr[3][2], arr[3][3], arr[3][4]);
 				
 			}
 			
