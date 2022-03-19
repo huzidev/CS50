8106b03dd43937e8bffbd2f0f6e8d6a4ea5e244f
huzidev <huzaifa.iqdev@gmail.com>
2022-03-19 15:07:38 +0500

    size of array

--- C/DSA/Array1/2D-Array/2D-ArrayProblem.c
+++ C/DSA/Array1/2D-Array/2D-ArrayProblem.c
@@ -58,7 +58,11 @@ int main(void){
 			
 			for(int i = 0; i < 1; i++){
 				
+				int n = sizeof(arr)/(sizeof(arr[0]));
+				
 				printf("%d %d %d %d %d ", arr[0][0], arr[1][1], arr[2][2], arr[3][3], arr[4][4]);
+				
+				printf("Total elements in this case are %d ", n);
 				
 			}
 			
