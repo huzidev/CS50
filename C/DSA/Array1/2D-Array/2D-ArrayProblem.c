2d2e22b48cf28f9c5b02907a2ac7da6b7cfccb99
huzidev <huzaifa.iqdev@gmail.com>
2022-03-19 07:06:35 +0500

    2d-array

--- C/DSA/Array1/2D-Array/2D-ArrayProblem.c
+++ C/DSA/Array1/2D-Array/2D-ArrayProblem.c
@@ -24,19 +24,19 @@ int main(void){
 			}
 		
 		}
-		
 		printf("\nTotal element are %d ", size-1);
+		
+		printf("********** MENU **********");
+		printf("\n1.Press 'a' for printing only elements diagonally in ascending order ");
+		printf("\n1.Press 'd' for printing only elements diagonally in descending order ");
+		
 		
 		printf("\nAfter appyling algorithm! : ");
 		
-		for(int i = 0; i < row; i++){
-		
-			for(int j = 0; j < columns; j++){
+		for(int i = 0; i < 1; i++){
 			
-				printf("%d ", arr[0, 0][1, 1]);
+			printf("%d %d %d %d %d ", arr[0][0], arr[1][1], arr[2][2], arr[3][3], arr[4][4]);
 			
-			}
-		
 		}
 		
 		printf("\nDo you wanna continue? \n");
