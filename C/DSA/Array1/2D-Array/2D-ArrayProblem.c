2d4f16b415abeccbad79f60e4c329708237b6f73
huzidev <huzaifa.iqdev@gmail.com>
2022-03-19 06:28:48 +0500

    algorithm for 2d-array

---
+++ C/DSA/Array1/2D-Array/2D-ArrayProblem.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <cs50.c>
+
+int main(void){
+	
+	do{
+		
+		int row = 5;
+		
+		int columns = 5;
+		
+		int arr[5][5] = {{1, 2, 3, 4, 5}, {6, 7, 8, 9, 10}, {11, 12, 13, 14, 15}, {16, 17, 18, 19, 20}, {21, 22, 23, 24, 25}}; 
+		
+		printf("Before applying algorithm! ");
+		for(int i = 0; i < row; i++){
+		
+			for(int j = 0; j < columns; j++){
+			
+			printf("%d ", arr[i][j]);
+			
+			}
+		
+		}
+		
+		printf("\nDo you wanna continue? \n");
+	}
+		
+	while( 'y' == getch() );
+	
+	return 0;
+	
+}
