f7a27f3e106447f19c4e67b3f6b4f69409b288c8
huzidev <huzaifa.iqdev@gmail.com>
2022-03-19 06:29:35 +0500

    algorithm for printing data

--- C/DSA/Array1/2D-Array/2D-ArrayProblem.c
+++ C/DSA/Array1/2D-Array/2D-ArrayProblem.c
@@ -11,7 +11,8 @@ int main(void){
 		
 		int arr[5][5] = {{1, 2, 3, 4, 5}, {6, 7, 8, 9, 10}, {11, 12, 13, 14, 15}, {16, 17, 18, 19, 20}, {21, 22, 23, 24, 25}}; 
 		
-		printf("Before applying algorithm! ");
+		printf("Before applying algorithm! : ");
+		
 		for(int i = 0; i < row; i++){
 		
 			for(int j = 0; j < columns; j++){
@@ -21,6 +22,8 @@ int main(void){
 			}
 		
 		}
+		
+		printf("After appyling algorithm! : ")
 		
 		printf("\nDo you wanna continue? \n");
 	}
